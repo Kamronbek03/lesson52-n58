340b617f726ed8b5f59ab1bb44b0e9881c826857
Kamronbek03 <kamronb3k03@gmail.com>
2024-05-29 02:56:52 +0500

    the end

--- js/main.js
+++ js/main.js
@@ -183,3 +183,383 @@
 // time1.previusSecund();
 
 // N5 //
+// class Shape {
+//   constructor(color, filled) {
+//     this.color = color;
+//     this.filled = filled;
+//   }
+
+//   getColor() {
+//     return this.color;
+//   }
+
+//   setColor(newColor) {
+//     this.color = newColor;
+//   }
+
+//   isFilled() {
+//     return this.filled;
+//   }
+
+//   setFilled(newFilled) {
+//     this.filled = newFilled;
+//   }
+// }
+// class Circle extends Shape {
+//   constructor(color, filled, radius) {
+//     super(color, filled);
+//     this.radius = radius;
+//   }
+
+//   getRadius() {
+//     return this.radius;
+//   }
+
+//   setRadius(newRadius) {
+//     this.radius = newRadius;
+//   }
+
+//   getArea() {
+//     return Math.PI * this.radius ** 2;
+//   }
+
+//   getPerimeter() {
+//     return 2 * Math.PI * this.radius;
+//   }
+// }
+
+// class Rectangle extends Shape {
+//   constructor(color, filled, width, height) {
+//     super(color, filled);
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   getWidth() {
+//     return this.width;
+//   }
+
+//   setWidth(newWidth) {
+//     this.width = newWidth;
+//   }
+
+//   getHeight() {
+//     return this.height;
+//   }
+
+//   setHeight(newHeight) {
+//     this.height = newHeight;
+//   }
+
+//   getArea() {
+//     return this.width * this.height;
+//   }
+
+//   getPerimeter() {
+//     return 2 * (this.width + this.height);
+//   }
+// }
+
+// const redCircle = new Circle("red", true, 5);
+// console.log("Circle radius:", redCircle.getRadius());
+// console.log("Circle area:", redCircle.getArea());
+// console.log("Circle perimeter:", redCircle.getPerimeter());
+
+// const blueRectangle = new Rectangle("blue", false, 4, 6);
+// console.log("Rectangle width:", blueRectangle.getWidth());
+// console.log("Rectangle height:", blueRectangle.getHeight());
+// console.log("Rectangle area:", blueRectangle.getArea());
+// console.log("Rectangle perimeter:", blueRectangle.getPerimeter());
+
+// N6 //
+// class Person {
+//   constructor(name, address) {
+//     this.name = name;
+//     this.address = address;
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+
+//   setName(newName) {
+//     this.name = newName;
+//   }
+
+//   getAddress() {
+//     return this.address;
+//   }
+
+//   setAddress(newAddress) {
+//     this.address = newAddress;
+//   }
+// }
+
+// class Student extends Person {
+//   constructor(name, address, faculty, year, university) {
+//     super(name, address);
+//     this.faculty = faculty;
+//     this.year = year;
+//     this.university = university;
+//   }
+
+//   getFaculty() {
+//     return this.faculty;
+//   }
+
+//   getYear() {
+//     return this.year;
+//   }
+
+//   getUniversity() {
+//     return this.university;
+//   }
+
+//   setFaculty(newFaculty) {
+//     this.faculty = newFaculty;
+//   }
+
+//   setYear(newYear) {
+//     this.year = newYear;
+//   }
+
+//   setUniversity(newUniversity) {
+//     this.university = newUniversity;
+//   }
+// }
+
+// class Employee extends Person {
+//   constructor(name, address, salary, work) {
+//     super(name, address);
+//     this.salary = salary;
+//     this.work = work;
+//   }
+
+//   getSalary() {
+//     return this.salary;
+//   }
+
+//   getWork() {
+//     return this.work;
+//   }
+
+//   setSalary(newSalary) {
+//     this.salary = newSalary;
+//   }
+
+//   setWork(newWork) {
+//     this.work = newWork;
+//   }
+// }
+
+// const student1 = new Student(
+//   "Jahongir",
+//   "Sayyoh street",
+//   "Computer Science",
+//   2,
+//   "INHA University"
+// );
+// console.log(student1.getName());
+// console.log(student1.getFaculty());
+
+// const employee1 = new Employee(
+//   "Baxodir",
+//   "Nurafshon street",
+//   60000,
+//   "Software Engineer"
+// );
+// console.log(employee1.getSalary());
+// console.log(employee1.getWork());
+
+// N7 //
+// class Animal {
+//   constructor(name, speed, weight) {
+//     this.name = name;
+//     this.speed = speed;
+//     this.weight = weight;
+//   }
+// }
+
+// class Mammal extends Animal {
+//   constructor(name, speed, weight, legs) {
+//     super(name, speed, weight);
+//     this.legs = legs;
+//   }
+
+//   getLegs() {
+//     return this.legs;
+//   }
+
+//   setLegs(newLegs) {
+//     this.legs = newLegs;
+//   }
+// }
+
+// class Cat extends Mammal {
+//   getSound() {
+//     return "Meow";
+//   }
+// }
+
+// class Dog extends Mammal {
+//   getSound() {
+//     return "Woof";
+//   }
+// }
+
+// class Fish extends Animal {
+//   constructor(name, speed, weight, size) {
+//     super(name, speed, weight);
+//     this.size = size;
+//   }
+
+//   getSize() {
+//     return this.size;
+//   }
+
+//   setSize(newSize) {
+//     this.size = newSize;
+//   }
+// }
+
+// class Whale extends Fish {
+//   getSound() {
+//     return "Poof";
+//   }
+// }
+
+// class Shark extends Fish {
+//   getSound() {
+//     return "Sheef";
+//   }
+// }
+
+// const cat1 = new Cat("Tom", 10, 5, 4);
+// console.log(cat1.getSound());
+// console.log(cat1.getLegs());
+
+// const shark1 = new Shark("Black", 30, 500, 5);
+// console.log(shark1.getSound());
+// console.log(shark1.getSize());
+
+// N8 //
+// class Object1 {
+//   static customKeys(obj) {
+//     return Object.keys(obj);
+//   }
+
+//   static customValues(obj) {
+//     return Object.values(obj);
+//   }
+
+//   static customEntries(obj) {
+//     return Object.entries(obj);
+//   }
+// }
+
+// class Number1 {
+//   static customIsInteger(num) {
+//     return Number.isInteger(num);
+//   }
+// }
+
+// class Array1 {
+//   static customIsArray(arr) {
+//     return Array.isArray(arr);
+//   }
+// }
+
+// const myObject = { a: 1, b: 2, c: 3 };
+// console.log("Keys:", Object1.customKeys(myObject));
+// console.log("Values:", Object1.customValues(myObject));
+// console.log("Entries:", Object1.customEntries(myObject));
+
+// const myNumber = 42;
+// console.log("Is integer:", Number1.customIsInteger(myNumber));
+
+// const myArray = [1, 2, 3];
+// console.log("Is array:", Array1.customIsArray(myArray));
+
+// N9 //
+// class CustomArray {
+//   static isNumberArray(arr) {
+//     return arr.every((element) => typeof element === "number");
+//   }
+
+//   static sum(arr) {
+//     return arr.reduce((total, element) => total + element, 0);
+//   }
+
+//   static max(arr) {
+//     return Math.max(...arr);
+//   }
+
+//   static min(arr) {
+//     return Math.min(...arr);
+//   }
+// }
+// const myArray = [10, 5, 8, 3, 12];
+// console.log("Is myArray a number array?", CustomArray.isNumberArray(myArray));
+// console.log("Sum of myArray:", CustomArray.sum(myArray));
+// console.log("Maximum element in myArray:", CustomArray.max(myArray));
+// console.log("Minimum element in myArray:", CustomArray.min(myArray));
+
+// N10 //
+class Person {
+  constructor(housePrice, housesNumber, carPrice, carsNumber, bankAccount) {
+    this.housePrice = housePrice;
+    this.housesNumber = housesNumber;
+    this.carPrice = carPrice;
+    this.carsNumber = carsNumber;
+    this.bankAccount = bankAccount;
+  }
+
+  getWealth() {
+    return (
+      this.housePrice * this.housesNumber +
+      this.carPrice * this.carsNumber +
+      this.bankAccount
+    );
+  }
+}
+
+class RichPerson extends Person {
+  constructor(
+    housePrice,
+    housesNumber,
+    carPrice,
+    carsNumber,
+    bankAccount,
+    companyPrice,
+    companiesNumber,
+    investigation
+  ) {
+    super(housePrice, housesNumber, carPrice, carsNumber, bankAccount);
+    this.companyPrice = companyPrice;
+    this.companiesNumber = companiesNumber;
+    this.investigation = investigation;
+  }
+
+  getWealth() {
+    return (
+      super.getWealth() +
+      this.companyPrice * this.companiesNumber +
+      this.investigation
+    );
+  }
+}
+
+const ordinaryPerson = new Person(200000, 1, 30000, 2, 100000);
+console.log("Ordinary person's wealth:", ordinaryPerson.getWealth());
+
+const richPerson = new RichPerson(
+  200000,
+  1,
+  30000,
+  2,
+  100000,
+  5000000,
+  3,
+  2000000
+);
+console.log("Rich person's wealth:", richPerson.getWealth());
